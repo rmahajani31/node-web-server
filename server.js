0cbfc9d82b1a82ce5044417306351a08738412db
Rishabh Mahajani <rmahajani3@gatech.edu>
2017-08-10 11:03:37 -0400

    Initializing node server project

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+
+const app = express();
+const hbs = require('hbs');
+
+hbs.registerPartials(`${__dirname}/views/partials`);
+app.use(express.static(`${__dirname}/public`));
+app.set('view engine', 'hbs');
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+app.use((request, response, next) => {
+    console.log(`Timestamp: ${new Date().toString()} Method: ${request.method} Url: ${request.url}`);
+    response.render('maintenance.hbs', {
+        pageTitle: 'Maintenance Page',
+        pageText: 'This site is currently under maintenance. Please come back later...'
+    });
+})
+
+app.get('/', (request, response) => {
+    response.render('home.hbs', {
+        pageTitle: 'Home Page',
+        message: "Welcome to the home page"
+    })
+});
+
+app.get('/about', (request, response) => {
+    response.render('about.hbs', {
+        pageTitle: 'About Page'
+    });
+});
+
+app.listen(3000, () => {
+    console.log("Server is up on port 3000");
+});
