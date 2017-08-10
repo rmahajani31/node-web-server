8a6954684f7a5950cbda93096d38eb9475b0127e
Rishabh Mahajani <rmahajani3@gatech.edu>
2017-08-10 11:21:37 -0400

    Adding modifications to deploy on heroku

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const hbs = require('hbs');
 
+const port = process.env.PORT || 3000;
 hbs.registerPartials(`${__dirname}/views/partials`);
 app.use(express.static(`${__dirname}/public`));
 app.set('view engine', 'hbs');
@@ -31,6 +32,6 @@ app.get('/about', (request, response) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log("Server is up on port 3000");
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
 });
